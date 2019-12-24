6d3b7ec5569860bc975c5beeeee9e345da370559
vpomo <pomogalov@gmail.com>
2019-12-24 22:58:51 +0300

    new test

--- migrations/2_deploy_contracts_sl.js
+++ migrations/2_deploy_contracts_sl.js
@@ -3,5 +3,5 @@ const GoodsManagement = artifacts.require('./GoodsManagement.sol');
 module.exports = (deployer) => {
     //http://www.onlineconversion.com/unix_time.htm
     var administrationWallet = "0xa57aeB1145Ab9ffCeBA6DC23bEF419570bD38110";
-    deployer.deploy(GoodsManagement, administrationWallet);
+    deployer.deploy(GoodsManagement);
 };
--- test/goods_management.js
+++ test/goods_management.js
@@ -4,20 +4,25 @@ var GoodsManagement = artifacts.require("./GoodsManagement.sol");
 var contractGM;
 
 
-contract('GoodsManagemen', (accounts) => {
+contract('GoodsManagement', (accounts) => {
     var owner = accounts[0]; // for test
     var decimal = Number(1e18);
 
     var buyEthOne = Number(0.02 * decimal);
 
-    it('should deployed contract GoodsManagemen', async () => {
+    it('should deployed contract GoodsManagement', async () => {
         assert.equal(undefined, contractGM);
-        contractGM = await GoodsManagemen.deployed();
+        contractGM = await GoodsManagement.deployed();
         assert.notEqual(undefined, contractGM);
     });
 
     it('get address contract', async () => {
         assert.notEqual(undefined, contractGM.address);
+    });
+
+    it('add goods contract', async () => {
+        await contractGM.setAdmin(accounts[0], true);
+        await contractGM.addToCatalog("goods 1", "info googs 1", 20, 1);
     });
 
 
